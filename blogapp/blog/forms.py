44ee87d17436fa562a5141e3893a1a5f2c94f30b
baamosk <bunth667@gmail.com>
2018-07-31 23:18:04 -0400

    better understanding of models

--- blogapp/blog/forms.py
+++ blogapp/blog/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 class CommentForm(forms.Form):
-
     name = forms.CharField(max_length=20,
         widget=forms.TextInput(attrs={'placeholder': 'NAME'}))
     comment = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'COMMENT'}))
